70d2cdd2cf9cb695834da18e560b707eedd5065f
nakachi <nakachi@ad-scope.co.jp>
2024-06-07 16:12:30 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <h1>test</h1>
       </header>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,6 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App />
-    <h1>test</h1>
   </React.StrictMode>
 );
 
