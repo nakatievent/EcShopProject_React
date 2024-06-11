4a60a110f8c09eef4f9600e8e38c70d350327ac3
nakachi <nakachi@ad-scope.co.jp>
2024-06-11 17:28:31 +0900

    ルーターを変更した

--- src/routers/Router.tsx
+++ src/routers/Router.tsx
@@ -7,7 +7,7 @@ import { createBrowserRouter } from 'react-router-dom'
 // import { SignIn } from "../pages/SignIn";
 // import { FailedLogin } from "../pages/FailedLogin";
 // import { Memo } from "../pages/Memo";
-import { Sample } from "../pages/Sample";
+// import { Sample } from "../pages/Sample";
 import { Login } from "../pages/Login";
 // import Top from "../pages/Top";
 // import { useAuth } from '../hooks/AuthContext';
