e358d240e92304b302f51abb9816e02cb2421b61
nakachi <nakachi@ad-scope.co.jp>
2024-06-13 14:08:06 +0900

    tailwindcss、eslint、prettierのインストールと設定を行った

---
+++ prettier.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  "printWidth": 120,
+  
+  // 二重引用符の代わりに一重引用符を使用
+  "singleQuote": true,
+  
+  // 文の最後にセミコロンを出力
+  "semi": false,
+  
+  // インデントレベル毎のスペース数
+  "tabWidth": 2,
+  
+  // スペースの代わりにタブを使用して行をインデント
+  "useTabs": true,
+  
+  // 配列、オブジェクト、関数の引数リストなどの末尾にカンマを付ける
+  "trailingComma": "none",
+  
+  // オブジェクトリテラル内の括弧間のスペースを出力
+  "bracketSpacing": true,
+  
+  // タグの終わりの括弧が改行されるかどうかを指定
+  "bracketSameLine": false
+}
