bb6623aa1f952ebfdbfc28d6da363e4e5460c14b
nakachi <nakachi@ad-scope.co.jp>
2024-06-13 14:09:12 +0900

    ログイン画面と新規登録画面のHTMLを実装した

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,53 +1,85 @@
-import React, { FC, useState, useMemo, useCallback, memo, useRef, useEffect } from 'react';
+import React, { FC, useState, useMemo, useCallback, memo, useRef, useEffect } from 'react'
 
-import { CustomButton } from '../components/atom/Button';
-import { CustomTextField } from '../components/atom/TextField';
-import { CustomTypography } from '../components/atom/Typography';
-import { BaseLayout } from '../components/layout/BaseLayout';
+import { Box } from '@mui/material'
+import { CustomButton } from 'components/atom/Button'
+import { CustomTextField } from 'components/atom/TextField'
+import { CustomTypography } from 'components/atom/Typography'
+import { BaseLayout } from 'components/layout/BaseLayout'
 
 export const Login: FC = () => {
-  
-  return (
-    <>
-    <BaseLayout>
-      <CustomTypography component="h1" variant="h5">
-        ログイン
-      </CustomTypography>
-      <CustomTextField
-        margin="normal"
-        required
-        fullWidth
-        id="email"
-        label="Email Address"
-        name="email"
-        autoComplete="email"
-        // value={email}
-        autoFocus
-        type="email"
-        // onChange={e => setEmail(e.target.value)}
-        />
-      <CustomTextField
-        margin="normal"
-        required
-        fullWidth
-        id="password"
-        label="Password"
-        name="password"
-        autoComplete="current-password"
-        // value={password}
-        type="password"
-        // onChange={e => setPassword(e.target.value)}
-        />
-      <CustomButton
-        type="submit"
-        fullWidth
-        variant="contained"
-        sx={{ mt: 3, mb: 2 }}
-        >
-        ログイン
-      </CustomButton>
-    </BaseLayout>
-    </>
-  );
+	return (
+		<>
+			<div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+				<div className="sm:mx-auto sm:w-full sm:max-w-sm">
+					<img
+						className="mx-auto h-10 w-auto"
+						src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+						alt="Your Company"
+					/>
+					<h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+						Sign in to your account
+					</h2>
+				</div>
 
+				<div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+					<form className="space-y-6" action="#" method="POST">
+						<div>
+							<label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+								Email address
+							</label>
+							<div className="mt-2">
+								<input
+									id="email"
+									name="email"
+									type="email"
+									autoComplete="email"
+									required
+									className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+								/>
+							</div>
+						</div>
+
+						<div>
+							<div className="flex items-center justify-between">
+								<label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+									Password
+								</label>
+								<div className="text-sm">
+									<a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+										Forgot password?
+									</a>
+								</div>
+							</div>
+							<div className="mt-2">
+								<input
+									id="password"
+									name="password"
+									type="password"
+									autoComplete="current-password"
+									required
+									className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+								/>
+							</div>
+						</div>
+
+						<div>
+							<button
+								type="submit"
+								className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+							>
+								Sign in
+							</button>
+						</div>
+					</form>
+
+					<p className="mt-10 text-center text-sm text-gray-500">
+						Not a member?{' '}
+						<a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+							Start a 14 day free trial
+						</a>
+					</p>
+				</div>
+			</div>
+		</>
+	)
 }
