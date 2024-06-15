e39f62a601398120f3667c59d58a0ae5e94ea0c3
nakachi <nakachi@ad-scope.co.jp>
2024-06-15 20:44:51 +0900

    商品詳細のページを追加

--- src/routers/Router.tsx
+++ src/routers/Router.tsx
@@ -1,20 +1,12 @@
 import React from 'react'
 import { createBrowserRouter } from 'react-router-dom'
-
-// import { Route, Routes } from "react-router-dom";
-// import { Login } from "../components/Login";
-// import { Test } from "../components/Test";
-// import { SignIn } from "../pages/SignIn";
-// import { FailedLogin } from "../pages/FailedLogin";
-// import { Memo } from "../pages/Memo";
-// import { Sample } from "../pages/Sample";
 import { Login } from 'pages/Login'
 import { Main } from 'pages/Main'
 import { MyPage } from 'pages/MyPage'
 import { ProductDetail } from 'pages/ProductDetail'
 import { Register } from 'pages/Register'
-// import Top from "../pages/Top";
-// import { useAuth } from '../hooks/AuthContext';
+import { Cart } from 'pages/Cart'
+
 
 export const Router = createBrowserRouter([
 	{
@@ -36,10 +28,10 @@ export const Router = createBrowserRouter([
 	{
 		path: '/register',
 		element: <Register />
-		// }, {
-		//   path: '/sign-in',
-		//   element: <SignIn />
-		// }, {
+	}, {
+		path: '/cart',
+		element: <Cart />
+	}, {
 		//   path: '/sign-up',
 		//   element: <SignUp />
 		// }, {
