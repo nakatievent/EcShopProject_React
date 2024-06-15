a2e2b6b0818d75d3a56b4d2efe2a8c46b9214080
nakachi <nakachi@ad-scope.co.jp>
2024-06-15 20:41:22 +0900

    ReactQueryクライアントを追加

---
+++ src/lib/reactQuery.tsx
@@ -0,0 +1,15 @@
+import { QueryClient } from '@tanstack/react-query'
+
+export const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			// throwOnError: true,
+			refetchOnWindowFocus: false,
+			retry: false,
+			staleTime: 1000 * 60
+		},
+		mutations: {
+			retry: false
+		}
+	}
+})
