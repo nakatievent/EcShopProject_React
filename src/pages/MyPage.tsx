c42465254394ba916e3736594ed6b09afd27a1f7
nakachi <nakachi@ad-scope.co.jp>
2024-06-15 20:45:54 +0900

    マイページを追加

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,15 +1,10 @@
-import React, { FC, useState, useMemo, useCallback, memo, useRef, useEffect } from 'react';
+import React, { FC, useState, useMemo, useCallback, memo, useRef, useEffect } from 'react'
 
-import { Box }              from '@mui/material';
-import { CustomButton }     from '../components/atom/Button';
-import { CustomTextField }  from '../components/atom/TextField';
-import { CustomTypography } from '../components/atom/Typography';
-import { BaseLayout }       from '../components/layout/BaseLayout';
+import { CustomButton } from '../components/atom/Button'
+import { CustomTextField } from '../components/atom/TextField'
+import { CustomTypography } from '../components/atom/Typography'
+import { BaseLayout } from '../components/layout/BaseLayout'
 
 export const MyPage: FC = () => {
-  return (
-    <>
-
-    </>
-  )
+	return <></>
 }
