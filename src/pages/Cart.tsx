289ac23cd83bd1e241677080fe9315b1083ce2f9
nakachi <nakachi@ad-scope.co.jp>
2024-06-25 11:50:57 +0900

    カートのHTMLを仮実装

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -30,126 +30,82 @@ export const Cart: FC = () => {
 	const [open, setOpen] = useState<boolean>(true)
 
 	return (
-		<Transition show={open}>
-			<Dialog className="relative z-10" onClose={setOpen}>
-				<TransitionChild
-					enter="ease-in-out duration-500"
-					enterFrom="opacity-0"
-					enterTo="opacity-100"
-					leave="ease-in-out duration-500"
-					leaveFrom="opacity-100"
-					leaveTo="opacity-0"
-				>
-					<div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-				</TransitionChild>
-
-				<div className="fixed inset-0 overflow-hidden">
-					<div className="absolute inset-0 overflow-hidden">
-						<div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
-							<TransitionChild
-								enter="transform transition ease-in-out duration-500 sm:duration-700"
-								enterFrom="translate-x-full"
-								enterTo="translate-x-0"
-								leave="transform transition ease-in-out duration-500 sm:duration-700"
-								leaveFrom="translate-x-0"
-								leaveTo="translate-x-full"
-							>
-								<DialogPanel className="pointer-events-auto w-screen max-w-md">
-									<div className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
-										<div className="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
-											<div className="flex items-start justify-between">
-												<DialogTitle className="text-lg font-medium text-gray-900">Shopping cart</DialogTitle>
-												<div className="ml-3 flex h-7 items-center">
-													<button
-														type="button"
-														className="relative -m-2 p-2 text-gray-400 hover:text-gray-500"
-														onClick={() => setOpen(false)}
-													>
-														<span className="absolute -inset-0.5" />
-														<span className="sr-only">Close panel</span>
-														<XMarkIcon className="h-6 w-6" aria-hidden="true" />
-													</button>
-												</div>
-											</div>
-
-											<div className="mt-8">
-												<div className="flow-root">
-													<ul role="list" className="-my-6 divide-y divide-gray-200">
-														{products.map((product) => (
-															<li key={product.id} className="flex py-6">
-																<div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-																	<img
-																		src={product.imageSrc}
-																		alt={product.imageAlt}
-																		className="h-full w-full object-cover object-center"
-																	/>
-																</div>
-
-																<div className="ml-4 flex flex-1 flex-col">
-																	<div>
-																		<div className="flex justify-between text-base font-medium text-gray-900">
-																			<h3>
-																				<a href={product.href}>{product.name}</a>
-																			</h3>
-																			<p className="ml-4">{product.price}</p>
-																		</div>
-																		<p className="mt-1 text-sm text-gray-500">{product.color}</p>
-																	</div>
-																	<div className="flex flex-1 items-end justify-between text-sm">
-																		<p className="text-gray-500">Qty {product.quantity}</p>
-
-																		<div className="flex">
-																			<button
-																				type="button"
-																				className="font-medium text-indigo-600 hover:text-indigo-500"
-																			>
-																				Remove
-																			</button>
-																		</div>
-																	</div>
-																</div>
-															</li>
-														))}
-													</ul>
-												</div>
-											</div>
-										</div>
-
-										<div className="border-t border-gray-200 px-4 py-6 sm:px-6">
-											<div className="flex justify-between text-base font-medium text-gray-900">
-												<p>Subtotal</p>
-												<p>$262.00</p>
-											</div>
-											<p className="mt-0.5 text-sm text-gray-500">Shipping and taxes calculated at checkout.</p>
-											<div className="mt-6">
-												<a
-													href="#"
-													className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
-												>
-													Checkout
-												</a>
-											</div>
-											<div className="mt-6 flex justify-center text-center text-sm text-gray-500">
-												<p>
-													or{' '}
-													<button
-														type="button"
-														className="font-medium text-indigo-600 hover:text-indigo-500"
-														onClick={() => setOpen(false)}
-													>
-														Continue Shopping
-														<span aria-hidden="true"> &rarr;</span>
-													</button>
-												</p>
-											</div>
-										</div>
-									</div>
-								</DialogPanel>
-							</TransitionChild>
+		<div className="mx-auto w-11/12 mt-10">
+			<div className="flex flex-col lg:flex-row bg-white rounded-lg overflow-hidden">
+				{/* カート */}
+				<div className="w-full lg:w-2/3 p-4">
+					<h2 className="text-2xl font-bold mb-6">カート</h2>
+					<div className="flex items-center border-b pb-4 mb-4">
+						<div className="w-24 h-24 bg-gray-200 flex-shrink-0"></div>
+						<div className="ml-4 flex-grow">
+							<h3 className="text-sm font-semibold sm:text-lg">ブランド名</h3>
+							<p className="text-sm text-gray-600 sm:text-lg">商品名</p>
+							<p className="text-sm text-gray-800 font-bold sm:text-lg">3,773円</p>
+						</div>
+						<div className="flex items-center">
+							<input
+								type="number"
+								value="1"
+								className="w-16 p-1 border rounded-md text-center"
+							></input>
+							<button className="ml-4 text-red-500">削除</button>
 						</div>
 					</div>
 				</div>
-			</Dialog>
-		</Transition>
+				{/* カートとお会計情報の間 */}
+				<div className="w-full lg:w-1/3 p-4">
+					<div className="flex justify-between">
+						<h2 className="text-2xl font-bold mb-4">合計</h2>
+						<p className="text-xl font-semibold mb-4">3,773円</p>
+					</div>
+					<h2 className="text-lg mb-4">会員のお客様さま</h2>
+					<div className="mb-4">
+						<label
+							htmlFor="login-id"
+							className="block text-gray-700 mb-2"
+						>
+							ログインID
+						</label>
+						<input
+							type="text"
+							id="login-id"
+							className="w-full p-2 border rounded-md"
+							placeholder="ログインID"
+						></input>
+					</div>
+					<div className="mb-4">
+						<label
+							htmlFor="password"
+							className="block text-gray-700 mb-2"
+						>
+							パスワード
+						</label>
+						<input
+							type="password"
+							id="password"
+							className="w-full p-2 border rounded-md"
+							placeholder="パスワード"
+						></input>
+					</div>
+					<div className="text-center my-7">
+						<a
+							href="#"
+							className="text-blue-500 text-sm"
+						>
+							ログインID、パスワードをお忘れの方
+						</a>
+					</div>
+					<div className="mb-4">
+						<button className="w-full p-2 bg-slate-900 text-white">ログインして注文手続きへ</button>
+					</div>
+					<div className="mb-4">
+						<button className="w-full p-2 bg-gray-500 text-white">会員登録して注文手続きへ</button>
+					</div>
+					<div>
+						<button className="w-full p-2 bg-white text-black border">会員登録しないで注文手続きへ</button>
+					</div>
+				</div>
+			</div>
+		</div>
 	)
 }
