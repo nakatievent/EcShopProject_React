56856cd79268e9bcadb48f6d8dc692e369a4fb81
nakachi <nakachi@ad-scope.co.jp>
2024-06-28 18:12:56 +0900

    決済フォームを作成途中

---
+++ src/components/organisms/PaymentForm.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from "react";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "components/organisms/CheckoutForm";
+import { usePaymentIntent } from "hooks/useCreatePaymentIntent";
+
+
+export const PaymentForm: FC = () => {
+  const aaa: number = 200 // TODO: 後から動的に取得する
+  const { clientSecret, error, isLoading, appearance, options, stripePromise } = usePaymentIntent(aaa)
+
+	return (
+    <div className="App">
+      {clientSecret && (
+        <Elements options={options} stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
+      )}
+    </div>
+	)
+}
