bc6ca67997eab6d68b40bf32d546e13c7c442735
nakachi <nakachi@ad-scope.co.jp>
2024-07-01 14:07:48 +0900

    カートに関する処理を追加

--- src/hooks/CartContext.tsx
+++ src/hooks/CartContext.tsx
@@ -13,6 +13,7 @@ interface CartContextType {
   removeFromCart: (productId: number) => void;
   clearCart: () => void;
   cartTotal: number;
+  fixCartData: Array<Product|unknown>;
 }
 
 export const CartContext = createContext<CartContextType | undefined>(undefined);
@@ -28,22 +29,40 @@ export const CartProvider: FC<CartProviderProps> = ({ children }) => {
     saveCartToLocalStorage(cart);
   }, [cart]);
 
+  // カートに商品を追加
   const addToCart = (product: Product) => {
     setCart([...cart, product]);
   };
 
+  // カートから商品を削除
   const removeFromCart = (productId: number) => {
     setCart(cart.filter(item => item.id !== productId));
   };
 
+  // カートを空にする
   const clearCart = () => {
     setCart([]);
   };
 
+  // カートの合計金額
   const cartTotal = cart.reduce((total, item) => total + item.price, 0);
-
+  
+  // カート内の同じ商品をグルーピング
+  const groupByCartData = cart.reduce((previousValue: any, currentValue: any) => {
+    if (!previousValue[currentValue.name]) {
+      previousValue[currentValue.name] = { ...currentValue, count: 1 }
+    } else {
+      previousValue[currentValue.name].price += currentValue.price
+      previousValue[currentValue.name].count += 1;
+    }
+    return previousValue
+  }, {})  
+  
+  // グループ化したカートデータを配列に変換
+  const fixCartData = Object.values(groupByCartData);
+  
   return (
-    <CartContext.Provider value={{ cart, addToCart, removeFromCart, clearCart, cartTotal }}>
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart, clearCart, cartTotal, fixCartData }}>
       {children}
     </CartContext.Provider>
   )
--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,33 +1,32 @@
-import React, { FC, useState } from 'react'
-import { Dialog, DialogPanel, DialogTitle, Transition, TransitionChild } from '@headlessui/react'
-import { XMarkIcon } from '@heroicons/react/24/outline'
-
-const products = [
-	{
-		id: 1,
-		name: 'Throwback Hip Bag',
-		href: '#',
-		color: 'Salmon',
-		price: '$90.00',
-		quantity: 1,
-		imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg',
-		imageAlt: 'Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt.'
-	},
-	{
-		id: 2,
-		name: 'Medium Stuff Satchel',
-		href: '#',
-		color: 'Blue',
-		price: '$32.00',
-		quantity: 1,
-		imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg',
-		imageAlt: 'Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.'
-	}
-	// More products...
-]
+import React, { FC, useState, useContext } from 'react'
+import { useNavigate } from "react-router-dom";
+import { CartContext } from 'hooks/CartContext';
 
 export const Cart: FC = () => {
+	const navigate = useNavigate()
 	const [open, setOpen] = useState<boolean>(true)
+	const [number, setNumber] = useState(0);
+
+
+	const cartContext = useContext(CartContext)
+
+	if (!cartContext) {
+		throw new Error('CartContext not found')
+	}
+	
+	const { cart, addToCart, removeFromCart, clearCart, cartTotal, fixCartData } = cartContext;
+
+	console.log(fixCartData)
+
+	const onClickButton = (destination: string) => {
+		navigate(destination);
+	};
+	
+	const handleInputChange = (event: any) => {
+		console.log()
+		addToCart(event.target.value)
+    // 実行したい関数の内容をここに追加
+  };
 
 	return (
 		<div className="mx-auto w-11/12 mt-10">
@@ -35,28 +34,37 @@ export const Cart: FC = () => {
 				{/* カート */}
 				<div className="w-full lg:w-2/3 p-4">
 					<h2 className="text-2xl font-bold mb-6">カート</h2>
-					<div className="flex items-center border-b pb-4 mb-4">
-						<div className="w-24 h-24 bg-gray-200 flex-shrink-0"></div>
-						<div className="ml-4 flex-grow">
-							<h3 className="text-sm font-semibold sm:text-lg">ブランド名</h3>
-							<p className="text-sm text-gray-600 sm:text-lg">商品名</p>
-							<p className="text-sm text-gray-800 font-bold sm:text-lg">3,773円</p>
+					{/* TODO: fixCartDataは配列 */}
+					{fixCartData && fixCartData.map((product: any) => (
+						<div key={product.id} className="flex items-center border-b pb-4 mb-4">
+							<div className="w-24 h-24 bg-gray-200 flex-shrink-0"></div>
+							<div className="ml-4 flex-grow">
+								<h3 className="text-sm font-semibold sm:text-lg">ブランド名</h3>
+								<p className="text-sm text-gray-600 sm:text-lg">{product.name}</p>
+								<p className="text-sm text-gray-800 font-bold sm:text-lg">{product.price}円</p>
+							</div>
+							<div className="flex items-center">
+								<input
+									type="number"
+									value={product.count}
+									className="w-16 p-1 border rounded-md text-center"
+									onChange={handleInputChange}
+								></input>
+								<button
+									className="ml-4 text-red-500"
+									onClick={() => removeFromCart(product.id)}
+								>
+									削除
+								</button>
+							</div>
 						</div>
-						<div className="flex items-center">
-							<input
-								type="number"
-								value="1"
-								className="w-16 p-1 border rounded-md text-center"
-							></input>
-							<button className="ml-4 text-red-500">削除</button>
-						</div>
-					</div>
+					))}
 				</div>
 				{/* カートとお会計情報の間 */}
 				<div className="w-full lg:w-1/3 p-4">
 					<div className="flex justify-between">
 						<h2 className="text-2xl font-bold mb-4">合計</h2>
-						<p className="text-xl font-semibold mb-4">3,773円</p>
+						<p className="text-xl font-semibold mb-4">{cartTotal}円</p>
 					</div>
 					<h2 className="text-lg mb-4">会員のお客様さま</h2>
 					<div className="mb-4">
@@ -74,10 +82,7 @@ export const Cart: FC = () => {
 						></input>
 					</div>
 					<div className="mb-4">
-						<label
-							htmlFor="password"
-							className="block text-gray-700 mb-2"
-						>
+						<label htmlFor="password" className="block text-gray-700 mb-2">
 							パスワード
 						</label>
 						<input
@@ -88,21 +93,49 @@ export const Cart: FC = () => {
 						></input>
 					</div>
 					<div className="text-center my-7">
-						<a
-							href="#"
-							className="text-blue-500 text-sm"
-						>
+						<a href="#" className="text-blue-500 text-sm">
 							ログインID、パスワードをお忘れの方
 						</a>
 					</div>
 					<div className="mb-4">
-						<button className="w-full p-2 bg-slate-900 text-white">ログインして注文手続きへ</button>
+						<button
+							className="w-full p-2 bg-slate-900 text-white"
+							onClick={() => onClickButton("/login")}
+						>
+							ログインして注文手続きへ
+						</button>
 					</div>
 					<div className="mb-4">
-						<button className="w-full p-2 bg-gray-500 text-white">会員登録して注文手続きへ</button>
+						<button
+							className="w-full p-2 bg-gray-500 text-white"
+							onClick={() => onClickButton("/register")}
+						>
+							会員登録して注文手続きへ
+						</button>
 					</div>
 					<div>
-						<button className="w-full p-2 bg-white text-black border">会員登録しないで注文手続きへ</button>
+						<button
+							className="w-full p-2 bg-white text-black border"
+							onClick={() => onClickButton("/input-adress")}
+						>
+							会員登録しないで注文手続きへ
+						</button>
+					</div>
+					<div className="mt-4">
+						<button
+							className="w-full p-2 bg-white text-black border"
+							onClick={() => onClickButton("/payment")}
+						>
+							そのまま買う
+						</button>
+					</div>
+					<div className="mt-4">
+						<button
+							className="w-full p-2 bg-white text-black border"
+							onClick={() => clearCart()}
+						>
+							カート削除ボタン
+						</button>
 					</div>
 				</div>
 			</div>
