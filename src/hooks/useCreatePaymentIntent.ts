1ce99224b220ca4d801e4a47161af0ca2120e7d4
nakachi <nakachi@ad-scope.co.jp>
2024-07-03 16:28:06 +0900

    簡易的な修正

--- src/hooks/useCreatePaymentIntent.ts
+++ src/hooks/useCreatePaymentIntent.ts
@@ -3,12 +3,13 @@ import * as api from 'api/PaymentApi'
 import { useQuery } from '@tanstack/react-query'
 import { Stripe, loadStripe, Appearance, StripeElementsOptions } from "@stripe/stripe-js";
 
+// TODO: 直し方が分からないので一旦公開キーを直書きしています
 const stripePromise = loadStripe("pk_test_51Mh5klHJVPH1M5dhqhz8JrT28zYHB4qMm82srnsoUQgR3apo3Ejy6TSgihrwRZ5BsSFnQBZyRoS93jXRSAobjcad00n9xO218T");
 
 export const usePaymentIntent = (total_price: number) => {
   const [clientSecret, setClientSecret] = useState<string|undefined>();
   
-  const { data, error, isError, isLoading, isFetched } = useQuery({
+  const { data, error, isLoading } = useQuery({
     queryKey: ['createPaymentIntent'],
     queryFn: () => api.createPaymentIntent({ total_price: total_price }).then((response) => setClientSecret(response.data))
   })
