7a39a179cbbe0147d14e8895a2aac94562933dbf
nakachi <nakachi@ad-scope.co.jp>
2024-07-09 16:48:19 +0900

    不要な記述の削除&バリデーション項目の追加

--- src/pages/InputAdress.tsx
+++ src/pages/InputAdress.tsx
@@ -10,10 +10,6 @@ export const InputAdress: FC = () => {
 	const [navigate, setNavigate] = useState<boolean>(false)
 	const [postalCode, setPostalCode] = useState<string>('')
 
-	// カードの有効期限（年・月)
-	const months: Array<string> = [...Array(12)].map((_, i) => `${1 + i}月`)
-	const years: Array<string> = [...Array(10)].map((_, i) => `${new Date().getFullYear() + i}年`)
-
 	// カート情報
 	const cartContext = useContext(CartContext)
 	const { cart, cartTotal } = cartContext
@@ -48,7 +44,6 @@ export const InputAdress: FC = () => {
 
 	if (navigate) {
 		return <Navigate to="/payment-form" state={{ state: inputData }} />
-		// return <Navigate to="/confirm" state={{ state: inputData }} />
 	}
 
 	return (
@@ -59,7 +54,7 @@ export const InputAdress: FC = () => {
 					<div className="grid grid-cols-1 gap-2">
 						<label className="flex items-center text-[#2c3e50]">お名前</label>
 						<div className="grid grid-cols-2 gap-2">
-							<div className="flex flex-col">
+							<div className="flex flex-col gap-2">
 								<input
 									className="bg-white border border-gray-300 p-3 rounded-md"
 									type="text"
@@ -74,7 +69,7 @@ export const InputAdress: FC = () => {
 								/>
 								{errors.firstName && <span className="text-red-500 text-sm">{errors.firstName.message}</span>}
 							</div>
-							<div className="flex flex-col">
+							<div className="flex flex-col gap-2">
 								<input
 									className="bg-white border border-gray-300 p-3 rounded-md"
 									type="text"
@@ -111,17 +106,23 @@ export const InputAdress: FC = () => {
 					<div className="grid grid-cols-1 gap-2">
 						<label className="flex items-center text-[#2c3e50]">住所</label>
 						<input
-							name="address"
 							value={address}
 							readOnly
 							className="bg-white border border-gray-300 p-3 rounded-md"
 							type="text"
+							{...register('address', {
+								required: '必須項目です',
+								maxLength: {
+									value: 255,
+									message: '最大255文字です'
+								}
+							})}
 						/>
+						{errors.address && <span className="text-red-500 text-sm">{errors.address.message}</span>}
 					</div>
 					<div className="grid grid-cols-1 gap-2">
 						<label className="flex items-center text-[#2c3e50]">電話番号</label>
 						<input
-							// name="phone"
 							className="bg-white border border-gray-300 p-3 rounded-md"
 							type="text"
 							{...register('phone', {
@@ -132,8 +133,8 @@ export const InputAdress: FC = () => {
 								}
 							})}
 						/>
+						{errors.phone && <span className="text-red-500 text-sm">{errors.phone.message}</span>}
 					</div>
-					{errors.phone && <span className="text-red-500 text-sm">{errors.phone.message}</span>}
 					<div className="grid grid-cols-1 gap-2">
 						<label className="flex items-center text-[#2c3e50]">お届け日</label>
 						<input
@@ -163,8 +164,8 @@ export const InputAdress: FC = () => {
 								}
 							})}
 						/>
+						{errors.email && <span className="text-red-500 text-sm">{errors.email.message}</span>}
 					</div>
-					{errors.email && <span className="text-red-500 text-sm">{errors.email.message}</span>}
 					<div className="grid grid-cols-1 gap-2">
 						<label className="flex items-center text-[#2c3e50]">メールアドレス（確認用）</label>
 						<input
@@ -185,55 +186,7 @@ export const InputAdress: FC = () => {
 								}
 							})}
 						/>
-					</div>
-					{errors.confirmEmail && <span className="text-red-500 text-sm">{errors.confirmEmail.message}</span>}
-					<div className="space-y-4">
-						<div className="flex justify-between items-center">
-							<label className="flex items-center text-[#2c3e50]">お支払い方法</label>
-							<button
-								type="button"
-								className="bg-[#4a90e2] text-white px-4 py-2 rounded-md hover:bg-[#357ABD] transition-all duration-200"
-							>
-								変更
-							</button>
-						</div>
-						<div className="grid grid-cols-1 gap-2">
-							<label className="flex items-center text-[#2c3e50]">カード番号</label>
-							<input
-								name="cardNumber"
-								className="bg-white border border-gray-300 p-3 rounded-md"
-								type="text"
-								placeholder="例: 12345678901234"
-							/>
-						</div>
-						<div className="grid grid-cols-1 gap-2">
-							<label className="flex items-center text-[#2c3e50]">セキュリティコード</label>
-							<input
-								name="securityCode"
-								className="bg-white border border-gray-300 p-3 rounded-md"
-								type="text"
-								placeholder="例: 123"
-							/>
-						</div>
-						<div className="grid grid-cols-1 gap-2">
-							<label className="flex items-center text-[#2c3e50]">有効期限</label>
-							<div className="grid grid-cols-2 gap-2">
-								<select name="expiryMonth" className="bg-white border border-gray-300 p-3 rounded-md">
-									{months.map((month) => (
-										<option key={month} value={month}>
-											{month}
-										</option>
-									))}
-								</select>
-								<select name="expiryYear" className="bg-white border border-gray-300 p-3 rounded-md">
-									{years.map((year) => (
-										<option key={year} value={year}>
-											{year}
-										</option>
-									))}
-								</select>
-							</div>
-						</div>
+						{errors.confirmEmail && <span className="text-red-500 text-sm">{errors.confirmEmail.message}</span>}
 					</div>
 					<div>
 						<div className="border-t border-gray-300 my-6"></div>
